9b36b64cb7381bc3511bb4e5659eea3517b512f5
zoky4 <zoky4@hotmail.com>
2015-11-05 16:20:21 +0100

    dodan edittext

--- app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
+++ app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
@@ -3,10 +3,12 @@ package hr.foi.air.webshopapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
+    EditText txtBox;
 
 
     @Override
@@ -14,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnAdd = (Button) findViewById(R.id.btnDodaj);
+        txtBox = (EditText) findViewById(R.id.editText);
     }
 }
