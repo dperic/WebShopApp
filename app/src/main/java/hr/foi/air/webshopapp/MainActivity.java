27578d3715e959a7e2d34a0683ad5e308d1e27f4
zoky4 <zoky4@hotmail.com>
2015-11-05 19:14:12 +0100

    dodane tablice u bazu i radi dodavanje korisnika

--- app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
+++ app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import hr.foi.air.webshopapp.dbmodule.UserTable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
+    Button btnUpdate;
+    EditText imeUnos;
+    Button btnrefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnUpdate = (Button) findViewById(R.id.btnDodaj);
+        btnUpdate.setOnClickListener(this);
+        imeUnos = (EditText) findViewById(R.id.editText);
+        btnrefresh =  (Button) findViewById(R.id.btnRefresh);
+        btnrefresh.setOnClickListener(this);
     }
 
 
@@ -43,5 +56,17 @@ public class MainActivity extends AppCompatActivity {
         //Temporary shows message that click is handled.
         Toast.makeText(this, "Menu item " + item.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnRefresh:
+                break;
+
+            case R.id.btnDodaj:
+                UserTable.UpdateUser(imeUnos.getText().toString());
+                break;
+        }
     }
 }
