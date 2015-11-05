4da94dcbe183703d18e8fa83be1226ab7ffea05d
zoky4 <zoky4@hotmail.com>
2015-11-05 16:13:42 +0100

    dodan gumb

--- app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
+++ app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
@@ -2,12 +2,17 @@ package hr.foi.air.webshopapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = (Button) findViewById(R.id.btnDodaj);
     }
 }
