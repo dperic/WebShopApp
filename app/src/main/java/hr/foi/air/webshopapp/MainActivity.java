d8f5b5a225d1b19e71c7d1d4fd2b5d626efd27fd
robertkc <robi_kc7@hotmail.com>
2015-11-05 17:48:45 +0100

    dodan db modul i AA

--- app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
+++ app/src/main/java/hr/foi/air/webshopapp/MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
