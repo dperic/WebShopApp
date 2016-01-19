c6482a6fe7ba21be9afb4c0d6bda3e011e8f574d
robertkc <robi_kc7@hotmail.com>
2016-01-19 20:37:24 +0100

    dodatak

---
+++ app/src/main/java/hr/foi/air/webshopapp/activity/LoadImageActivity.java
@@ -0,0 +1,34 @@
+package hr.foi.air.webshopapp.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import hr.foi.air.webshopapp.R;
+import hr.foi.air.webshopapp.adapter.ImageAdapter;
+
+
+public class LoadImageActivity extends Activity {
+
+    private String[] imageURLArray = new String[]{
+            "http://farm8.staticflickr.com/7315/9046944633_881f24c4fa_s.jpg",
+            "http://farm4.staticflickr.com/3777/9049174610_bf51be8a07_s.jpg",
+            "http://farm8.staticflickr.com/7324/9046946887_d96a28376c_s.jpg",
+            "http://farm3.staticflickr.com/2828/9046946983_923887b17d_s.jpg",
+            "http://farm4.staticflickr.com/3810/9046947167_3a51fffa0b_s.jpg",
+            "http://farm4.staticflickr.com/3773/9049175264_b0ea30fa75_s.jpg",
+            "https://z-1-scontent-fra3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/" +
+                    "12249763_10208301005585772_4441425751759241085_n.jpg?oh=a0186b143e170f27d00a9fb99017f05f&oe=5705AF2B"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_image);
+
+        ListView listView = (ListView)this.findViewById(R.id.listView);
+        ImageAdapter imageAdapter = new ImageAdapter(this, R.layout.image_item, imageURLArray);
+        listView.setAdapter(imageAdapter);
+    }
+
+}
---
+++ app/src/main/java/hr/foi/air/webshopapp/adapter/ImageAdapter.java
@@ -0,0 +1,80 @@
+package hr.foi.air.webshopapp.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.URL;
+
+import hr.foi.air.webshopapp.R;
+
+public class ImageAdapter extends ArrayAdapter<String> {
+    private String[] imageURLArray;
+    private LayoutInflater inflater;
+
+    public ImageAdapter(Context context, int textViewResourceId,
+                        String[] imageArray) {
+        super(context, textViewResourceId, imageArray);
+        // TODO Auto-generated constructor stub
+
+        inflater = ((Activity) context).getLayoutInflater();
+        imageURLArray = imageArray;
+    }
+    private static class ViewHolder {
+        String imageURL;
+        ImageView imageView;
+        Bitmap bitmap;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.image_item, null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.testImage);
+            convertView.setTag(viewHolder);
+        }
+
+        viewHolder = (ViewHolder) convertView.getTag();
+        viewHolder.imageURL = imageURLArray[position];
+        new DownloadAsyncTask().execute(viewHolder);
+        return convertView;
+    }
+
+    private class DownloadAsyncTask extends AsyncTask<ViewHolder, Void, ViewHolder> {
+
+        @Override
+        protected ViewHolder doInBackground(ViewHolder... params) {
+            // TODO Auto-generated method stub
+            //load image directly
+            ViewHolder viewHolder = params[0];
+            try {
+                URL imageURL = new URL(viewHolder.imageURL);
+                viewHolder.bitmap = BitmapFactory.decodeStream(imageURL.openStream());
+            } catch (IOException e) {
+                // TODO: handle exception
+                Log.e("error", "Downloading Image Failed");
+                viewHolder.bitmap = null;
+            }
+
+            return viewHolder;
+        }
+
+        @Override
+        protected void onPostExecute(ViewHolder result) {
+            result.imageView.setImageBitmap(result.bitmap);
+        }
+    }
+}
